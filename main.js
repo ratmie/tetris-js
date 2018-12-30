6703983e94c98704fe47378b4d8971e6ee255aa1
Jumpei Takegata <takegata.j@gmail.com>
2018-12-30 23:42:39 +0900

    add Print Block

--- main.js
+++ main.js
@@ -3,16 +3,29 @@ let ctx;
 const DOT_SIZE = 20;
 const FIELD_WIDTH = 10;
 const FIELD_HEIGHT = 20;
-const SCREEN_WIDTH = (FIELD_WIDTH + 2) * DOT_SIZE;
-const SCREEN_HEIGHT = (FIELD_HEIGHT + 2) * DOT_SIZE;
+const SCREEN_WIDTH = FIELD_WIDTH * DOT_SIZE;
+const SCREEN_HEIGHT = FIELD_HEIGHT * DOT_SIZE;
 var posX = 3;
-var posY = 10;
+var posY = 0;
 var rightPressed = false;
 var leftPressed = false;
 let frame = 0;
 const GREY = "rgb(128,128,128)";
 const BLACK = "rgb(225,225,225)";
-const RED = "rgb(225,0,0)";
+const WHITE = "rgb(5,5,5)";
+const RED = "rgb(240,0,0)";
+const BLUE = "rgb(0,0,240)";
+var block;
+
+//テトリミノの定義 4 * 4の領域内の座標を示す。
+const BlockType = [{
+	color: RED,
+	cells: [{ x: 2, y: 0 }, { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 1, y: 2 }]
+},
+{
+	color: BLUE,
+	cells: [{ x: 1, y: 0 }, { x: 2, y: 0 }, { x: 1, y: 1 }, { x: 1, y: 2 }]
+}];
 
 const init = () => {
 	canvas = document.getElementById('maincanvas');
@@ -21,6 +34,8 @@ const init = () => {
 	canvas.height = SCREEN_HEIGHT;
 	ctx.fillStyle = BLACK;
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	block = new Block(BlockType[1]);
+
 	requestAnimationFrame(update);
 };
 
@@ -28,75 +43,76 @@ const update = () => {
 	requestAnimationFrame(update);
 
 	frame++;
-	if ((frame % 10) == 0) {
+	if ((frame % 30) == 0) {
 		console.log(frame);
-		// if (posY  < FIELD_HEIGHT) {
-		// 	posY += 1;
-		// }
-		if(rightPressed && posX < FIELD_WIDTH) {
+		if (posY < FIELD_HEIGHT - 1) {
+			posY += 1;
+		}
+		if (rightPressed) {
 			posX += 1;
-		} else if(leftPressed && posX > 1) {
+		} else if (leftPressed) {
 			posX -= 1;
 		}
 		render();
 	}
 };
 
-const  render = () => {
+const render = () => {
 	console.log('x:' + posX + ', y:' + posY);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle = BLACK;
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	printFrame();
-	printCell(posX, posY, RED)
-
-	// ctx.beginPath();
-	// ctx.moveTo( posX * DOT_SIZE, posY);
-	// ctx.lineTo(DOT_SIZE + movex, 20);
-	// ctx.lineTo(DOT_SIZE + movex, 120);
-	// ctx.lineTo(20 + movex, 120);
-	// ctx.closePath();
-	// ctx.stroke();
-
-
-};
-
-const printFrame = () => {
-	for(let y = 0; y < (FIELD_HEIGHT + 2); y++) {
-		printCell(0, y);
-		printCell(FIELD_WIDTH + 1, y);
-	}
-	for(let x = 1; x < (FIELD_WIDTH + 1); x++) {
-		printCell(x, FIELD_HEIGHT + 1);
-	}
-	printCell(1, 0);
-	printCell(2, 0);
-	printCell(FIELD_WIDTH - 1, 0);
-	printCell(FIELD_WIDTH, 0);
+	// printFrame();
+	//printCell(posX, posY, RED)
+	block.print(posX, posY);
 };
 
 const printCell = (x, y, color = BLACK) => {
-	ctx.fillStyle = color;
-	ctx.fillRect(x * DOT_SIZE, y * DOT_SIZE, DOT_SIZE, DOT_SIZE);
-	ctx.fillSyele = GREY;
-	ctx.strokeRect(x * DOT_SIZE, y * DOT_SIZE, DOT_SIZE, DOT_SIZE);
+	if (checkRange(x, y)) {
+		ctx.fillStyle = color;
+		ctx.fillRect(x * DOT_SIZE, y * DOT_SIZE, DOT_SIZE, DOT_SIZE);
+		ctx.fillSyele = GREY;
+		ctx.strokeRect(x * DOT_SIZE, y * DOT_SIZE, DOT_SIZE, DOT_SIZE);
+	} else {
+		console.log('false x:' + x + ' y:' + y);
+	}
 };
 
-const keyDownHandler= e => {
-	if(e.keyCode == 39) {
+const checkRange = (x, y) => {
+	if ((0 <= x) && (x < FIELD_WIDTH) && (0 <= y) && (y < FIELD_HEIGHT)) {
+		return true;
+	}
+	return false;
+}
+
+const keyDownHandler = e => {
+	if (e.keyCode == 39) {
 		rightPressed = true;
 	} else if (e.keyCode == 37) {
 		leftPressed = true;
 	}
 };
 
-const keyUpHandler= e => {
-	if(e.keyCode == 39) {
+const keyUpHandler = e => {
+	if (e.keyCode == 39) {
 		rightPressed = false;
 	} else if (e.keyCode == 37) {
 		leftPressed = false;
 	}
 };
+
+
+class Block {
+	constructor(type) {
+		this.color = type.color;
+		this.cells = type.cells;
+	};
+	print(x, y) {
+		for(let cell of this.cells) {
+			printCell(x + cell.x, y + cell.y, this.color);
+		}
+	};
+}
 
 window.addEventListener('load', init);
 document.addEventListener('keydown', keyDownHandler, false);
